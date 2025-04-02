c9d854132bc0f3f1fabd5747a3d794872390cc2f
Kirti7310 <karapurkarkirti@gmail.com>
2025-04-02 16:10:21 +0530

    Initial commit

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,67 @@
+dmx.config({
+  "main_page": {
+    "category_data_detail": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "ID"
+        },
+        {
+          "type": "text",
+          "name": "catg_name"
+        },
+        {
+          "type": "text",
+          "name": "catg_desc"
+        },
+        {
+          "type": "number",
+          "name": "price"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail1": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "ID"
+        },
+        {
+          "type": "text",
+          "name": "catg_name"
+        },
+        {
+          "type": "text",
+          "name": "catg_desc"
+        },
+        {
+          "type": "number",
+          "name": "price"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail2": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "ID"
+        },
+        {
+          "type": "text",
+          "name": "catg_name"
+        },
+        {
+          "type": "text",
+          "name": "catg_desc"
+        },
+        {
+          "type": "number",
+          "name": "price"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
